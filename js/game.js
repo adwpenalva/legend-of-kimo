aa25f02d8d3492fa87ce2cf7d8a951fb94d96223
ynnep <adwpenalva@gmail.com>
2020-02-06 12:26:32 +0000

    fixed score not showing on gameover screen, files cleared and organized

---
+++ js/game.js
@@ -0,0 +1,117 @@
+class Game {
+  constructor($canvas) {
+    this.menu = new Menu(this);
+    this.canvas = $canvas;
+    this.swordspeed = 1.5;
+    this.context = $canvas.getContext('2d');
+    this.kimo = new Player(this);
+    this.swordsArrayVertical = [];
+    this.swordsArrayHorizontal = [];
+    this.swordsArrayVerticalUp = [];
+    this.swordsArrayHorizontalRight = [];
+    this.gameIsRunning = false;
+    this.image = new Image();
+    this.image.src = './images/iceboard.jpg';
+    this.createSwords();
+    this.xWindowCenter = window.width / 2;
+    this.yWindowCenter = window.height / 2;
+    this.score = 0;
+    this.scoreBoard = new Scoreboard(this);
+  }
+
+  createSwords() {
+    for (let i = 0; i < 300; i++) {
+      const sword = new SwordH(this, 600 + i * 100);
+      this.swordsArrayHorizontal.push(sword);
+      const swordV = new SwordV(this, -4400 + i * 100 * -1);
+      this.swordsArrayVertical.push(swordV);
+      const swordVUp = new SwordVUp(this, 8400 + i * 100);
+      this.swordsArrayVerticalUp.push(swordVUp);
+      const swordHRight = new SwordHRight(this, -12600 + i * 100 * -1);
+      this.swordsArrayHorizontalRight.push(swordHRight);
+    }
+  }
+
+  startMenu() {
+    this.menu.drawStartImage();
+  }
+
+  startGame() {
+    this.gameIsRunning = true;
+    this.loop();
+  }
+
+  cleanCanvas = () => {
+    this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height);
+  };
+
+  paint = () => {
+    this.cleanCanvas();
+    this.context.drawImage(this.image, 0, 0, 600, 400);
+    this.kimo.drawImage();
+
+    for (let sword of this.swordsArrayHorizontal) {
+      sword.drawImage();
+    }
+
+    for (let swordV of this.swordsArrayVertical) {
+      swordV.drawImage();
+    }
+
+    for (let swordVUp of this.swordsArrayVerticalUp) {
+      swordVUp.drawImage();
+    }
+
+    for (let swordHRight of this.swordsArrayHorizontalRight) {
+      swordHRight.drawImage();
+    }
+
+    this.scoreBoard.paint();
+  };
+
+  runLogic = () => {
+    for (let sword of this.swordsArrayHorizontal) {
+      sword.runLogic();
+      sword.checkCollision();
+    }
+    for (let swordV of this.swordsArrayVertical) {
+      swordV.runLogic();
+      swordV.checkCollision();
+    }
+    for (let swordVUp of this.swordsArrayVerticalUp) {
+      swordVUp.runLogic();
+      swordVUp.checkCollision();
+    }
+    for (let swordHRight of this.swordsArrayHorizontalRight) {
+      swordHRight.runLogic();
+      swordHRight.checkCollision();
+    }
+  };
+
+  loop = () => {
+    this.score++;
+    //console.log(this.score);
+    this.paint();
+    this.runLogic();
+    this.kimo.update();
+    this.swordspeed *= 1.00025;
+
+    if (this.gameIsRunning) {
+      window.requestAnimationFrame(this.loop);
+    }
+  };
+
+  reset() {
+    this.swordspeed = 1.5;
+    this.swordsArrayVertical = [];
+    this.swordsArrayHorizontal = [];
+    this.swordsArrayVerticalUp = [];
+    this.swordsArrayHorizontalRight = [];
+    this.kimo.positionX = 300;
+    this.kimo.positionY = 200;
+    this.score = 0;
+    this.kimo.velX = 0;
+    this.kimo.velY = 0;
+    this.createSwords();
+  }
+}
---
+++ js/menu.js
@@ -0,0 +1,74 @@
+//let audioElement = new Audio();
+
+class Menu {
+  constructor(game) {
+    this.game = game;
+    this.canvas = $canvas;
+    this.screen = 'title';
+    this.context = $canvas.getContext('2d');
+    this.image = new Image();
+    this.image.src = './images/profile.png';
+    this.lore = new Image();
+    this.lore.src = './images/lore.png';
+    this.instructions = new Image();
+    this.instructions.src = './images/instructions.png';
+    this.gameover = new Image();
+    this.gameover.src = './images/game-over.png';
+    this.audioElement = new Audio();
+    this.audioElement.src = './music/background-music.wav';
+    this.commands();
+  }
+
+  drawStartImage() {
+    this.context.drawImage(this.image, 0, 0, 600, 400);
+    this.screen = 'lore';
+  }
+
+  drawLore() {
+    this.context.drawImage(this.lore, 0, 0, 600, 400);
+    this.screen = 'instructions';
+  }
+
+  drawInstructions() {
+    this.context.drawImage(this.instructions, 0, 0, 600, 400);
+    this.screen = 'game';
+  }
+
+  drawGameover() {
+    this.context.drawImage(this.gameover, 0, 0, 600, 400);
+    this.context.fillStyle = 'white';
+    this.context.font = '46px fixedsys';
+    this.context.fillText(`${this.game.score}`, 286, 186);
+    this.screen = 'gameover';
+  }
+
+  commands() {
+    window.addEventListener('keydown', event => {
+      switch (event.keyCode) {
+        case 13:
+          if (this.screen === 'game') {
+            this.playMusic();
+            this.game.gameIsRunning = true;
+            this.game.startGame();
+            this.screen = 'playing';
+          } else if (this.screen === 'lore') {
+            this.drawLore();
+          } else if (this.screen === 'instructions') {
+            this.drawInstructions();
+          } else if (this.screen === 'gameover') {
+            this.game.reset();
+            this.drawStartImage();
+          }
+          break;
+      }
+    });
+  }
+
+  playMusic() {
+    this.audioElement.play();
+  }
+
+  stopMusic() {
+    this.audioElement.pause();
+  }
+}
